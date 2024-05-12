692f006536c1d8ab8faf9e09c03ab033516f24ce
Atakan Yaşar <atakany357@gmail.com>
2024-05-12 21:22:12 +0300

    create tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,38 +1,116 @@
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS DatabaseManager;
-DROP TABLE IF EXISTS Player;
-DROP TABLE IF EXISTS Coach;
+DROP TABLE IF EXISTS SessionSquads;
+DROP TABLE IF EXISTS MatchSession;
+DROP TABLE IF EXISTS PlayerPositions;
+DROP TABLE IF EXISTS PlayerTeams;
+DROP TABLE IF EXISTS Team;
+DROP TABLE IF EXISTS TvChannel;
+DROP TABLE IF EXISTS Stadium;
+DROP TABLE IF EXISTS Positions;
 DROP TABLE IF EXISTS Jury;
-SET FOREIGN_KEY_CHECKS = 1;
+DROP TABLE IF EXISTS Coach;
+DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS DatabaseManager;
 
 CREATE TABLE IF NOT EXISTS DatabaseManager (
-    username VARCHAR(512) PRIMARY KEY,
+    username VARCHAR(256) PRIMARY KEY,
     password LONGTEXT
 );
 
 CREATE TABLE IF NOT EXISTS Player (
-    username	VARCHAR(512) PRIMARY KEY,
-    password	VARCHAR(512),
-    name	VARCHAR(512),
-    surname	VARCHAR(512),
-    date_of_birth VARCHAR(512), 
+    username	VARCHAR(256) PRIMARY KEY,
+    password	VARCHAR(256),
+    name	VARCHAR(256),
+    surname	VARCHAR(256),
+    date_of_birth VARCHAR(256), 
     height REAL,
     weight REAL
 );
 
 CREATE TABLE IF NOT EXISTS Coach (
-	  username	VARCHAR(512) PRIMARY KEY,
-    password	VARCHAR(512),
-    name	VARCHAR(512),
-    surname	VARCHAR(512),
-    nationality VARCHAR(512) NOT NULL
+	username	VARCHAR(256) PRIMARY KEY,
+    password	VARCHAR(256),
+    name	VARCHAR(256),
+    surname	VARCHAR(256),
+    nationality VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Jury (
-	  username	VARCHAR(512) PRIMARY KEY,
-    password	VARCHAR(512),
-    name	VARCHAR(512),
-    surname	VARCHAR(512),
-    nationality VARCHAR(512) NOT NULL
+	username	VARCHAR(256) PRIMARY KEY,
+    password	VARCHAR(256),
+    name	VARCHAR(256),
+    surname	VARCHAR(256),
+    nationality VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Positions (
+    position_ID INTEGER PRIMARY KEY,
+    position_name VARCHAR(256)
+);
+
+CREATE TABLE IF NOT EXISTS Stadium (
+    stadium_ID INTEGER PRIMARY KEY,
+    stadium_name VARCHAR(256),
+    country VARCHAR(256),
+    UNIQUE (stadium_ID, stadium_name, country)
+);
+
+CREATE TABLE IF NOT EXISTS TvChannel (
+	channel_ID INTEGER PRIMARY KEY,
+    channel_name VARCHAR(256),
+    UNIQUE (channel_ID, channel_name)
+);
+
+CREATE TABLE IF NOT EXISTS Team 
+(
+    team_ID	INT PRIMARY KEY,
+    team_name	VARCHAR(256),
+    coach_username	VARCHAR(256) NOT NULL,
+    contract_start	VARCHAR(256),
+    contract_finish	VARCHAR(256),
+    channel_ID	INT,
+    channel_name	VARCHAR(256),
+    FOREIGN KEY (channel_ID, channel_name) REFERENCES TvChannel(channel_ID, channel_name),
+    FOREIGN KEY (coach_username) REFERENCES Coach(username)
+); 
+
+CREATE TABLE IF NOT EXISTS PlayerTeams (
+    player_teams_ID INTEGER PRIMARY KEY,
+	username VARCHAR(256),
+    team INTEGER,
+    FOREIGN KEY (username) REFERENCES Player(username),
+    FOREIGN KEY (team) REFERENCES Team(team_ID)
+);
+
+
+CREATE TABLE IF NOT EXISTS PlayerPositions (
+    player_positions_ID INTEGER PRIMARY KEY,
+    username VARCHAR(256),
+    position INTEGER,
+    FOREIGN KEY (username) REFERENCES Player(username),
+    FOREIGN KEY (position) REFERENCES Positions(position_ID)
+);
+
+CREATE TABLE IF NOT EXISTS SessionSquads (
+    squad_ID INTEGER PRIMARY KEY,
+    session_ID INTEGER,
+    played_player_username VARCHAR(256),
+    position_ID INTEGER,
+    FOREIGN KEY (played_player_username) REFERENCES Player(username),
+    FOREIGN KEY (position_ID) REFERENCES Positions(position_ID)
+);
+
+CREATE TABLE IF NOT EXISTS MatchSession (
+    session_ID INTEGER PRIMARY KEY,
+    team_ID INTEGER,
+    stadium_id INTEGER,
+    stadium_name VARCHAR(256),
+    stadium_country VARCHAR(256),
+    time_slot VARCHAR(256),
+    date VARCHAR(256),
+    assigned_jury_username VARCHAR(256) NOT NULL,
+    rating REAL,
+    FOREIGN KEY (team_ID) REFERENCES Team(team_ID),
+    FOREIGN KEY (stadium_id, stadium_name, stadium_country) REFERENCES Stadium(stadium_ID, stadium_name, country),
+    FOREIGN KEY (assigned_jury_username) REFERENCES Jury(username)
 );
 
--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,37 +1,232 @@
-INSERT INTO DatabaseManager (username, password) VALUES ('Kevin', 'Kevin');
-INSERT INTO DatabaseManager (username, password) VALUES ('Bob', 'Bob');
-INSERT INTO DatabaseManager (username, password) VALUES ('sorunlubirarkadas', 'muvaffakiyetsizleştiricileştiriveremeyebileceklerimizdenmişsinizcesine');
-INSERT INTO Jury (username, password, name, surname, nationality) VALUES ('o_ozcelik', 'ozlem.0347', 'Özlem', 'Özçelik', 'TR');
-INSERT INTO Jury (username, password, name, surname, nationality) VALUES ('m_sevinc', 'mehmet.0457', 'Mehmet', 'Sevinç', 'TR');
-INSERT INTO Jury (username, password, name, surname, nationality) VALUES ('e_sener', 'ertem.4587', 'Ertem', 'Şener', 'TR');
-INSERT INTO Jury (username, password, name, surname, nationality) VALUES ('s_engin', 'sinan.6893', 'Sinan', 'Engin', 'TR');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('d_santarelli', 'santa.really1', 'Daniele ', 'Santarelli', 'ITA');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('g_guidetti', 'guidgio.90', 'Giovanni ', 'Guidetti', 'ITA');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('f_akbas', 'a.fatih55', 'Ferhat ', 'Akbaş', 'TR');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('m_hebert', 'm.hebert45', 'Mike', 'Hebert', 'US');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('o_deriviere', 'oliviere_147', 'Oliviere', 'Deriviere', 'FR');
-INSERT INTO Coach (username, password, name, surname, nationality) VALUES ('a_derune', 'aderune_147', 'Amicia', 'DeRune', 'FR');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('g_orge', 'Go.1993', 'Gizem ', 'Örge', '26/04/1993', '170', '59');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('c_ozbay', 'Co.1996', 'Cansu ', 'Özbay', '17/10/1996', '182', '78');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('m_vargas', 'Mv.1999', 'Melissa ', 'Vargas', '16/10/1999', '194', '76');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('h_baladin', 'Hb.2007', 'Hande ', 'Baladın', '01/09/2007', '190', '81');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('a_kalac', 'Ak.1995', 'Aslı ', 'Kalaç', '13/12/1995', '185', '73');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('ee_dundar', 'Eed.2008', 'Eda Erdem ', 'Dündar', '22/06/2008', '188', '74');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('z_gunes', 'Zg.2008', 'Zehra ', 'Güneş', '07/07/2008', '197', '88');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('i_aydin', 'Ia.2007', 'İlkin ', 'Aydın', '05/01/2007', '183', '67');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('e_sahin', 'Es.2001', 'Elif ', 'Şahin', '19/01/2001', '190', '68');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('e_karakurt', 'Ek.2006', 'Ebrar ', 'Karakurt', '17/01/2006', '196', '73');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('s_akoz', 'Sa.1991', 'Simge ', 'Aköz', '23/04/1991', '168', '55');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('k_akman', 'Ka.2006', 'Kübra ', 'Akman', '13/10/2006', '200', '88');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('d_cebecioglu', 'Dc.2007', 'Derya ', 'Cebecioğlu', '24/10/2007', '187', '68');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('a_aykac', 'Aa.1996', 'Ayşe ', 'Aykaç', '27/02/1996', '176', '57');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_2826', 'P.45825', 'Brenda', 'Schulz', '13/12/2002', '193', '80');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_9501', 'P.99695', 'Erika', 'Foley', '21/12/1995', '164', '62');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_3556', 'P.49595', 'Andrea', 'Campbell', '26/4/1996', '185', '100');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_7934', 'P.24374', 'Beatrice', 'Bradley', '28/5/1997', '150', '57');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_4163', 'P.31812', 'Betsey', 'Lenoir', '7/5/1993', '156', '48');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_2835', 'P.51875', 'Martha', 'Lazo', '20/5/2001', '173', '71');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_8142', 'P.58665', 'Wanda', 'Ramirez', '3/1/1994', '183', '94');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_2092', 'P.16070', 'Eileen', 'Ryen', '21/6/2004', '188', '60');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_3000', 'P.73005', 'Stephanie', 'White', '19/5/2002', '193', '74');
-INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight) VALUES ('user_8323', 'P.33562', 'Daenerys', 'Targaryen', '16/9/2006', '222', '74');
+INSERT INTO DatabaseManager (username, password)
+VALUES
+    ('Kevin', 'Kevin'),
+    ('Bob', 'Bob'),
+    ('sorunlubirarkadas', 'muvaffakiyetsizleştiricileştiriveremeyebileceklerimizdenmişsinizcesine');
+
+INSERT INTO Jury (username, password, name, surname, nationality)
+VALUES
+    ('o_ozcelik', 'ozlem.0347', 'Özlem', 'Özçelik', 'TR'),
+    ('m_sevinc', 'mehmet.0457', 'Mehmet', 'Sevinç', 'TR'),
+    ('e_sener', 'ertem.4587', 'Ertem', 'Şener', 'TR'),
+    ('s_engin', 'sinan.6893', 'Sinan', 'Engin', 'TR');
+
+INSERT INTO Coach (username, password, name, surname, nationality)
+VALUES
+    ('d_santarelli', 'santa.really1', 'Daniele', 'Santarelli', 'ITA'),
+    ('g_guidetti', 'guidgio.90', 'Giovanni', 'Guidetti', 'ITA'),
+    ('f_akbas', 'a.fatih55', 'Ferhat', 'Akbaş', 'TR'),
+    ('m_hebert', 'm.hebert45', 'Mike', 'Hebert', 'US'),
+    ('o_deriviere', 'oliviere_147', 'Oliviere', 'Deriviere', 'FR'),
+    ('a_derune', 'aderune_147', 'Amicia', 'Derune', 'FR');
+
+INSERT INTO Player (username, password, name, surname, date_of_birth, height, weight)
+VALUES
+    ('g_orge', 'Go.1993', 'Gizem', 'Örge', '1993-04-26', 170, 59),
+    ('c_ozbay', 'Co.1996', 'Cansu', 'Özbay', '1996-10-17', 182, 78),
+    ('m_vargas', 'Mv.1999', 'Melissa', 'Vargas', '1999-10-16', 194, 76),
+    ('h_baladin', 'Hb.2007', 'Hande', 'Baladın', '2007-09-01', 190, 81),
+    ('a_kalac', 'Ak.1995', 'Aslı', 'Kalaç', '1995-12-13', 185, 73),
+    ('ee_dundar', 'Eed.2008', 'Eda Erdem', 'Dündar', '2008-06-22', 188, 74),
+    ('z_gunes', 'Zg.2008', 'Zehra', 'Güneş', '2008-07-07', 197, 88),
+    ('i_aydin', 'Ia.2007', 'İlkin', 'Aydın', '2007-01-05', 183, 67),
+    ('e_sahin', 'Es.2001', 'Elif', 'Şahin', '2001-01-19', 190, 68),
+    ('e_karakurt', 'Ek.2006', 'Ebrar', 'Karakurt', '2006-01-17', 196, 73),
+    ('s_akoz', 'Sa.1991', 'Simge', 'Aköz', '1991-04-23', 168, 55),
+    ('k_akman', 'Ka.2006', 'Kübra', 'Akman', '2006-10-13', 200, 88),
+    ('d_cebecioglu', 'Dc.2007', 'Derya', 'Cebecioğlu', '2007-10-24', 187, 68),
+    ('a_aykac', 'Aa.1996', 'Ayşe', 'Aykaç', '1996-02-27', 176, 57),
+    ('user_2826', 'P.45825', 'Brenda', 'Schulz', '2002-12-13', 193, 80),
+    ('user_9501', 'P.99695', 'Erika', 'Foley', '1995-12-21', 164, 62),
+    ('user_3556', 'P.49595', 'Andrea', 'Campbell', '1996-04-26', 185, 100),
+    ('user_7934', 'P.24374', 'Beatrice', 'Bradley', '1997-05-28', 150, 57),
+    ('user_4163', 'P.31812', 'Betsey', 'Lenoir', '1993-05-07', 156, 48),
+    ('user_2835', 'P.51875', 'Martha', 'Lazo', '2001-05-20', 173, 71),
+    ('user_8142', 'P.58665', 'Wanda', 'Ramirez', '1994-01-03', 183, 94),
+    ('user_2092', 'P.16070', 'Eileen', 'Ryen', '2004-06-21', 188, 60),
+    ('user_3000', 'P.73005', 'Stephanie', 'White', '2002-05-19', 193, 74),
+    ('user_8323', 'P.33562', 'Daenerys', 'Targaryen', '2006-09-16', 222, 74);
+
+INSERT INTO Positions (position_ID, position_name)
+VALUES
+    (0, 'Libero'),
+    (1, 'Setter'),
+    (2, 'Opposite hitter'),
+    (3, 'Outside hitter'),
+    (4, 'Middle blocker');
+
+INSERT INTO PlayerPositions (player_positions_ID, username, position)
+VALUES
+    (0, 'g_orge', 0),
+    (1, 'g_orge', 3),
+    (2, 'c_ozbay', 1),
+    (3, 'm_vargas', 2),
+    (4, 'h_baladin', 3),
+    (5, 'a_kalac', 4),
+    (6, 'ee_dundar', 4),
+    (7, 'z_gunes', 4),
+    (8, 'i_aydin', 1),
+    (9, 'i_aydin', 3),
+    (10, 'e_sahin', 1),
+    (11, 'e_sahin', 3),
+    (12, 'e_karakurt', 2),
+    (13, 'e_karakurt', 3),
+    (14, 's_akoz', 0),
+    (15, 'k_akman', 0),
+    (16, 'k_akman', 4),
+    (17, 'd_cebecioglu', 3),
+    (18, 'd_cebecioglu', 4),
+    (19, 'a_aykac', 0),
+    (20, 'user_2826', 2),
+    (21, 'user_2826', 1),
+    (22, 'user_9501', 0),
+    (23, 'user_9501', 4),
+    (24, 'user_3556', 1),
+    (25, 'user_3556', 0),
+    (26, 'user_7934', 4),
+    (27, 'user_7934', 2),
+    (28, 'user_4163', 3),
+    (29, 'user_4163', 0),
+    (30, 'user_2835', 2),
+    (31, 'user_2835', 3),
+    (32, 'user_8142', 1),
+    (33, 'user_8142', 3),
+    (34, 'user_2092', 4),
+    (35, 'user_2092', 2),
+    (36, 'user_3000', 1),
+    (37, 'user_3000', 4),
+    (38, 'user_8323', 3),
+    (39, 'user_8323', 2);
+
+INSERT INTO TvChannel (channel_ID, channel_name)
+VALUES
+    (0, 'BeIN Sports'),
+    (1, 'Digiturk'),
+    (2, 'TRT');
+
+INSERT INTO Team (team_ID, team_name, coach_username, contract_start, contract_finish, channel_ID, channel_name)
+VALUES
+    (0, 'Women A', 'd_santarelli', '2021-12-25', '2025-12-12', 0, 'BeIN Sports'),
+    (1, 'Women B', 'g_guidetti', '2021-09-11', '2026-09-11', 1, 'Digiturk'),
+    (2, 'U19', 'f_akbas', '2021-08-10', '2026-08-10', 0, 'BeIN Sports'),
+    (3, 'Women B', 'f_akbas', '2000-08-10', '2015-08-10', 1, 'Digiturk'),
+    (4, 'Women C', 'm_hebert', '2024-04-01', '2026-07-21', 1, 'Digiturk'),
+    (5, 'U19', 'o_deriviere', '2015-08-10', '2020-08-09', 2, 'TRT'),
+    (6, 'U19', 'a_derune', '2005-08-10', '2010-08-10', 2, 'TRT');
+
+INSERT INTO PlayerTeams (player_teams_ID, username, team)
+VALUES
+    (1, 'g_orge', 0),
+    (2, 'c_ozbay', 0),
+    (3, 'c_ozbay', 1),
+    (4, 'm_vargas', 0),
+    (5, 'm_vargas', 1),
+    (6, 'h_baladin', 0),
+    (7, 'h_baladin', 2),
+    (8, 'a_kalac', 0),
+    (9, 'a_kalac', 1),
+    (10, 'ee_dundar', 0),
+    (11, 'ee_dundar', 2),
+    (12, 'z_gunes', 0),
+    (13, 'z_gunes', 2),
+    (14, 'i_aydin', 1),
+    (15, 'i_aydin', 2),
+    (16, 'e_sahin', 0),
+    (17, 'e_karakurt', 0),
+    (18, 'e_karakurt', 2),
+    (19, 's_akoz', 0),
+    (20, 's_akoz', 1),
+    (21, 'k_akman', 0),
+    (22, 'k_akman', 2),
+    (23, 'd_cebecioglu', 0),
+    (24, 'd_cebecioglu', 1),
+    (25, 'a_aykac', 0),
+    (26, 'user_2826', 2),
+    (27, 'user_2826', 3),
+    (28, 'user_9501', 0),
+    (29, 'user_9501', 3),
+    (30, 'user_3556', 2),
+    (31, 'user_3556', 3),
+    (32, 'user_7934', 0),
+    (33, 'user_7934', 3),
+    (34, 'user_4163', 1),
+    (35, 'user_4163', 3),
+    (36, 'user_2835', 2),
+    (37, 'user_2835', 3),
+    (38, 'user_8142', 0),
+    (39, 'user_8142', 3),
+    (40, 'user_2092', 2),
+    (41, 'user_2092', 3),
+    (42, 'user_3000', 2),
+    (43, 'user_3000', 3),
+    (44, 'user_8323', 0),
+    (45, 'user_8323', 3);
+
+INSERT INTO SessionSquads (squad_ID, session_ID, played_player_username, position_ID)
+VALUES
+    (1, 0, 'g_orge', 0),
+    (2, 0, 'c_ozbay', 1),
+    (3, 0, 'm_vargas', 2),
+    (4, 0, 'h_baladin', 3),
+    (5, 0, 'a_kalac', 4),
+    (6, 0, 'ee_dundar', 4),
+    (7, 1, 'c_ozbay', 1),
+    (8, 1, 'm_vargas', 2),
+    (9, 1, 'i_aydin', 1),
+    (10, 1, 'a_kalac', 4),
+    (11, 1, 's_akoz', 0),
+    (12, 1, 'd_cebecioglu', 3),
+    (13, 2, 'g_orge', 3),
+    (14, 2, 'm_vargas', 2),
+    (15, 2, 'c_ozbay', 1),
+    (16, 2, 'a_kalac', 4),
+    (17, 2, 's_akoz', 0),
+    (18, 2, 'a_aykac', 0),
+    (19, 3, 'ee_dundar', 4),
+    (20, 3, 'h_baladin', 3),
+    (21, 3, 'z_gunes', 4),
+    (22, 3, 'i_aydin', 3),
+    (23, 3, 'e_karakurt', 2),
+    (24, 3, 'k_akman', 0),
+    (25, 4, 'user_2826', 2),
+    (26, 4, 'user_9501', 0),
+    (27, 4, 'user_3556', 1),
+    (28, 4, 'user_7934', 4),
+    (29, 4, 'user_4163', 3),
+    (30, 4, 'user_2835', 2),
+    (31, 5, 'user_2826', 1),
+    (32, 5, 'user_9501', 4),
+    (33, 5, 'user_3556', 0),
+    (34, 5, 'user_7934', 2),
+    (35, 5, 'user_4163', 0),
+    (36, 5, 'user_2835', 3),
+    (37, 6, 'g_orge', 0),
+    (38, 6, 'm_vargas', 2),
+    (39, 6, 'c_ozbay', 1),
+    (40, 6, 'a_kalac', 4),
+    (41, 6, 'e_karakurt', 3),
+    (42, 6, 'a_aykac', 0),
+    (43, 7, 'g_orge', 3),
+    (44, 7, 'm_vargas', 2),
+    (45, 7, 'c_ozbay', 1),
+    (46, 7, 'a_kalac', 4),
+    (47, 7, 'e_karakurt', 2),
+    (48, 7, 'a_aykac', 0);
+
+INSERT INTO Stadium (stadium_ID, stadium_name, country)
+VALUES
+    (0, 'Burhan Felek Voleybol Salonu', 'TR'),
+    (1, 'GD Voleybol Arena', 'TR'),
+    (2, 'Copper Box Arena', 'UK');
+
+INSERT INTO MatchSession (session_ID, team_ID, stadium_ID, stadium_name, stadium_country, time_slot, date, assigned_jury_username, rating)
+VALUES
+    (0, 0, 0, 'Burhan Felek Voleybol Salonu', 'TR', 1, '2024-03-10', 'o_ozcelik', 4.5),
+    (1, 1, 1, 'GD Voleybol Arena', 'TR', 1, '2024-04-03', 'o_ozcelik', 4.9),
+    (2, 0, 1, 'GD Voleybol Arena', 'TR', 3, '2024-04-03', 'o_ozcelik', 4.4),
+    (3, 2, 2, 'Copper Box Arena', 'UK', 2, '2024-04-03', 'm_sevinc', 4.9),
+    (4, 3, 2, 'Copper Box Arena', 'UK', 2, '2023-04-03', 'e_sener', 4.5),
+    (5, 3, 1, 'GD Voleybol Arena', 'TR', 1, '2023-05-27', 's_engin', 4.4),
+    (6, 0, 1, 'GD Voleybol Arena', 'TR', 1, '2022-09-01', 'm_sevinc', 4.6),
+    (7, 0, 2, 'Copper Box Arena', 'UK', 3, '2023-05-02', 'o_ozcelik', 4.7),
+    (8, 1, 0, 'Burhan Felek Voleybol Salonu', 'TR', 1, '2024-02-10', 'o_ozcelik', 4.5);
